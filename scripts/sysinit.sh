620423843014bf151a1e6eb25b524b7db6aa9529
Ryan J. Price <ryapric@gmail.com>
2022-01-01 18:40:46 -0600

    Download Go manually

--- scripts/sysinit.sh
+++ scripts/sysinit.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+goversion='1.17.5'
+
 # Using this as a temp fix for not having our own OCI image registry;
 # this stuff should go in a Containerfile
 apt-get update
 apt-get install -y \
   ca-certificates \
-  golang \
-  make
+  curl \
+  make \
+  tar
+
+curl -fsSL -o /tmp/go"${goversion}".tar.gz https://golang.org/dl/go"${goversion}".linux-amd64.tar.gz
+tar -C /usr/local -xzf /tmp/go"${goversion}".tar.gz
+go version
